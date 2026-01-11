c850a9823e39e0ec3e3bc9ad1012eb5764c31052
LandonM-byui <mah22009@byui.edu>
2026-01-10 18:46:10 -0700

    Added input for int grade with an output of letter grade and pass/fail confirmation

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+        string letter_grade = " ";
+        if (grade >= 90)
+        {
+            letter_grade = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter_grade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter_grade = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter_grade = "D";
+        }
+        else
+        {
+            letter_grade = "F";
+        }
+        Console.Write($"Your letter grade for your class is a {letter_grade}. ");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congrats! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately, you didn't pass. You'll surely pass it next time!");
+        }
+
     }
 }
