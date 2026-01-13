9393e1ce9181c91e22dccdd288bad6d3dedbb72c
LandonM-byui <mah22009@byui.edu>
2026-01-13 13:36:37 -0700

    Added a user input to create a list of numbers then print the max, sum, and average of the list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int input = 1;
+        int currentMax = 0;
+        Console.WriteLine("Enter a List of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.Write("Enter number: ");
+            input = int.Parse(Console.ReadLine());
+            if (input != 0)
+            {
+                if (input > currentMax)
+                {
+                    currentMax = input;
+                }
+                numbers.Add(input);
+            }
+        } while (input != 0);
+        float sum = 0;
+        foreach (float num in numbers)
+        {
+            sum += num;
+        }
+        float average = sum / numbers.Count;
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {currentMax}");
+        
     }
 }
