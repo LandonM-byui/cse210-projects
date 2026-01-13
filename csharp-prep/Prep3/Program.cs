ba5083272886d7e2361747b432c5481b1fde076c
LandonM-byui <mah22009@byui.edu>
2026-01-13 13:10:06 -0700

    Created a guess the number high or low game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        string gameRun = "Yes";
+
+        do
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1,101);
+            int answer;
+            do
+            {
+                Console.Write("What is your guess? ");
+                answer = int.Parse(Console.ReadLine());
+                if (answer > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (answer < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+            }
+            while (answer != number);
+
+            Console.WriteLine("You got it!");
+
+            Console.Write("Do you want to continue? ");
+            gameRun = Console.ReadLine();
+        } while (gameRun == "yes");
+    } 
 }
