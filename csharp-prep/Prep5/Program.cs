59c8a20c34eccdcee1bfa36ca74468c315217fb1
LandonM-byui <mah22009@byui.edu>
2026-01-14 20:54:31 -0700

    Created functions to facilitate user name, age, and a squared favorite number

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Globalization;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program");
+    }
+
+    static string PromtUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine()); 
+    }
+
+    static void PromptUserBirthYear(out int year)
+    {
+        Console.Write("Please enter your birth year: ");
+        year = int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int num)
+    {
+        return num * num;
+    }
+
+    static void DisplayResults(string name, int num, int year)
+    {
+        Console.WriteLine($"{name}, the square of your of your number is {num}");
+        Console.WriteLine($"{name}, you will turn {2026 - year} this year.");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        string name = PromtUserName();
+        int number = PromptUserNumber();
+        int year; 
+        PromptUserBirthYear(out year);
+        int square = SquareNumber(number);
+        DisplayResults(name, square, year);
+
     }
 }
