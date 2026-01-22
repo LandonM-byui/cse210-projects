7e40bb1352fa90b85281e9ee2ba4965616d69125
LandonM-byui <mah22009@byui.edu>
2026-01-22 15:56:02 -0700

    Added classes for Job and Resume with display functions for each

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job software = new Job();
+        software._company = "Direwolf Digital";
+        software._jobTitle = "Software Engineer";
+        software._startYear = 2024;
+        software._endYear = 2025;
+
+        Job boardGame = new Job();
+        boardGame._company = "Ledger Games";
+        boardGame._jobTitle = "Game Tester";
+        boardGame._startYear = 2026;
+        boardGame._endYear = 2027;
+        software.DisplayJobDetails();
+        boardGame.DisplayJobDetails();
+        
+        Resume my_resume = new Resume();
+        my_resume._name = "Landon Mahaffey";
+        my_resume._jobs.Add(software);
+        my_resume._jobs.Add(boardGame);
+        my_resume.DisplayResume();
     }
 }
