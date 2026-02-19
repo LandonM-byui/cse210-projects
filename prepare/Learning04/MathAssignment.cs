5ce32e6322c0b13a3bc875986ef216de1d95f89f
LandonM-byui <mah22009@byui.edu>
2026-02-19 16:53:59 -0700

    Created a parent class of Assignment and child classes of MathAssignment and WritingAssignment

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,16 @@
+public class MathAssignment : Assignment
+{
+    private string _textbookSection;
+    private string _problems;
+
+    public MathAssignment(string name, string topic, string text, string problems) : base(name, topic)
+    {
+        _textbookSection = text;
+        _problems = problems;
+    }
+
+    public string GetHomeworkList()
+    {
+        return $"Textbook Section: {_textbookSection} | Problems: {_problems}";
+    }
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment problem = new MathAssignment("Landon", "sql", "2", "8-19");
+        Console.WriteLine(problem.getSummary());
+        Console.WriteLine(problem.GetHomeworkList());
+
+        WritingAssignment problem2 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(problem2.getSummary());
+        Console.WriteLine(problem2.GetWritingInformation());        
     }
 }
